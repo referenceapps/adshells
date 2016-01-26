e5805739a49b2b1cae6f1087e9f63e4254b1924c
--get <fausto.ribechini@it.ibm.com>
2016-01-26 12:45:54 -0800

    reenter usage of two cf

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ echo "*******************************"
 mkdir /tmp/cf
 wget -O /tmp/cf$$.tgz 'https://cli.run.pivotal.io/stable?release=linux64-binary&version=6.13.0&source=github-rel'
 tar -C /tmp/cf -xzf /tmp/cf$$.tgz
-export PATH=/tmp/cf:$PATH
+#export PATH=/tmp/cf:$PATH
 
 echo "************************************"
 echo "*** Install Active Deploy Plugin ***"
@@ -104,17 +104,17 @@ echo "**   ENTERING ACTIVE DEPLOY            **"
 echo "*****************************************"
 routeflag=" --no-route"
 cf push ${CF_APP}${count}${routeflag} -m 256MB
-cf active-deploy-create $old $new --rampup 1m --test 1m --rampdown 1m -l ${CF_APP}_deploy
+/tmp/cf active-deploy-create $old $new --rampup 1m --test 1m --rampdown 1m -l ${CF_APP}_deploy
 sleep 240
 # show the status of completed deployment
-cf active-deploy-show ${CF_APP}_deploy
+/tmp/cf active-deploy-show ${CF_APP}_deploy
 cf apps
 
 # delete apps
 cf delete -f $old
 
 # delete deployment
-cf active-deploy-delete ${CF_APP}_deploy
+/tmp/cf active-deploy-delete ${CF_APP}_deploy
 fi
 fi
 
